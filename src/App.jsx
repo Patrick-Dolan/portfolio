56e6ae3ae6d8d2fba8679695da5799a8267d9bc5
Patrick Dolan <dolanp1992@gmail.com>
2024-06-05 08:44:32 -0700

    set up basic page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import Footer from "./components/sections/Footer";
+
 function App() {
   return (
-    <h1 className="text-center text-4xl">App</h1>
+    <div className="flex flex-col min-h-screen">
+      <header>
+        <h1 className="text-center text-4xl">Patrick Dolan</h1>
+      </header>
+      <main className="flex-grow">
+        <p className="text-center">Main content.</p>
+      </main>
+      <Footer />
+    </div>
   )
 }
 
