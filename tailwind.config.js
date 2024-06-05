1764e13118a13f20ee1da1ee8a63f6662b5b9cd1
Patrick Dolan <dolanp1992@gmail.com>
2024-06-05 11:16:15 -0700

    add primary and secondary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#062f4f",
+        "secondary": "#b82601",
+      }
+    },
   },
   plugins: [],
 }
