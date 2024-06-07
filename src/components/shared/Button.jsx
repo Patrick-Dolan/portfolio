8134e25e4a2bc37d3c54a188e1a7b0b0da6de470
Patrick Dolan <dolanp1992@gmail.com>
2024-06-07 10:49:35 -0700

    add optional icon to button

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,13 +1,16 @@
 import PropTypes from "prop-types";
 
-function Button({ text, type, onClick, className }) {
+function Button({ text, type, onClick, className, icon }) {
   return (
     <button 
       className={`uppercase tracking-wider font-bold min-w-40 text-sm py-4 max-h-12 flex items-center justify-center text-white bg-secondary rounded-xl ${className}`} 
       type={type} 
       onClick={onClick}
     >
-      {text}
+      <div className="flex items-center gap-2">
+        {text}
+        {icon && icon}
+      </div>
     </button>
   )
 }
@@ -17,6 +20,7 @@ Button.propTypes = {
   type: PropTypes.string.isRequired,
   onClick: PropTypes.func,
   className: PropTypes.string,
+  icon: PropTypes.node,
 }
 
 export default Button;
