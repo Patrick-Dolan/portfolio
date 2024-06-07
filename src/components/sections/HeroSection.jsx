24dedde936b280fb99219b6bb33ec8bd9ba035f4
Patrick Dolan <dolanp1992@gmail.com>
2024-06-07 09:47:06 -0700

    update tag line

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -11,7 +11,7 @@ function HeroSection() {
           <h1 className="text-4xl">Patrick Dolan</h1>
           <p className="text-xl">I&apos;m a Frontend Developer</p>
         </div>
-        <p className="text-sm mt-4 mb-6">I build websites with a mix of code and calculations. When I&apos;m not writing code, I&apos;m usually getting lost in a good book.</p>
+        <p className="text-sm mt-4 mb-6">Building digital experiences with code and problem-solving. When I&apos;m not writing code, I&apos;m usually getting lost in a good book.</p>
         <div className="flex gap-2">
           <Button text="Contact Me" type="button" className="mx-auto"/>
           <Button text="Resume" type="button" className="mx-auto"/>
