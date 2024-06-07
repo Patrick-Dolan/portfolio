e1b6ed2043d0f8739b40b46639ef26fce3f5f8d9
Patrick Dolan <dolanp1992@gmail.com>
2024-06-07 10:50:01 -0700

    replace image and add icons to buttons

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -1,20 +1,22 @@
 import Button from "../shared/Button";
+import MailIcon from "../../assets/Icons/MailIcon";
+import DescriptionIcon from "../../assets/Icons/DescriptionIcon";
 
 function HeroSection() {
   return (
     <article className="flex items-center justify-center text-center">
       <div>
         {/* TODO replace placeholder image */}
-        <img src="/assets/silly-face.jpg" alt="" className="mx-auto my-8 rounded-3xl max-w-80" />
+        <img src="/assets/sillypic.jpg" alt="" className="mx-auto my-8 rounded-3xl max-w-80" />
         <div className="space-y-1">
           <p className="text-xl">Hello, my name is</p>
           <h1 className="text-4xl">Patrick Dolan</h1>
           <p className="text-xl">I&apos;m a Frontend Developer</p>
         </div>
-        <p className="text-sm mt-4 mb-6">Building digital experiences with code and problem-solving. When I&apos;m not writing code, I&apos;m usually getting lost in a good book.</p>
+        <p className="text-sm mt-4 mb-6 max-w-screen-sm">Building digital experiences with code and problem-solving. When I&apos;m not writing code, I&apos;m usually getting lost in a good book.</p>
         <div className="flex gap-2">
-          <Button text="Contact Me" type="button" className="mx-auto"/>
-          <Button text="Resume" type="button" className="mx-auto"/>
+          <Button text="Contact Me" type="button" className="mx-auto" icon={<MailIcon height="18px" width="18px" />}/>
+          <Button text="Resume" type="button" className="mx-auto" icon={<DescriptionIcon height="18px" width="18px" />}/>
         </div>
       </div>
     </article>
