f0dd4475518b839a8535a2cd65c454e51bb3d158
Patrick Dolan <dolanp1992@gmail.com>
2024-06-10 11:32:13 -0700

    add skills section to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import NavBar from "./components/navigation/NavBar";
 import Footer from "./components/sections/Footer";
 import HeroSection from "./components/sections/HeroSection";
+import SkillsSection from "./components/sections/SkillsSection";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       </header>
       <main className="flex-grow">
         <HeroSection />
+        <SkillsSection />
       </main>
       <Footer />
     </div>
