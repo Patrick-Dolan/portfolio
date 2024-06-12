49ab2bef1d0056a255f5624b4303b4b0dbbda296
Patrick Dolan <dolanp1992@gmail.com>
2024-06-12 16:47:49 -0700

    refactor cards

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,24 +1,25 @@
 import PropTypes from "prop-types";
-import Button from "../shared/Button";
+// import Button from "../shared/Button";
 import WebIcon from "../../assets/Icons/WebIcon";
 import GitHubIcon from "../../assets/Icons/GitHubIcon";
 
 function ProjectCard({ project }) {
-  const { imgPath, title, description, liveURL, codeURL} = project;
+  const { backgroundClass, title, description, liveURL, codeURL, logoPath } = project;
 
   return (
-    <article className="flex flex-col text-center bg-primary-light rounded-xl shadow-lg">
-      <img src={imgPath} alt="" className="mx-auto w-full max-h-60 object-cover rounded-t-xl" />
-      <div className="px-6 py-8">
-        <h3 className="text-2xl mb-3 font-bold">{title}</h3>
+    <article className={`flex flex-col justify-end text-center shadow-lg max-w-87.5 h-125 bg-white bg-cover bg-no-repeat ${backgroundClass}`}>
+      <img src={logoPath} alt={`${title} logo picture`} className="m-auto max-w-52" />
+      <div className="px-6 py-8 bg-primary-light bg-opacity-50 backdrop-blur-md">
+        <h3 className="text-xl mb-3 font-bold">{title}</h3>
         <p>{description}</p>
-        <div className="mt-8 space-y-2">
-          <a className="inline-block" href={liveURL} target="_blank">
-            <Button text="View Website" type="button" icon={<WebIcon />} />
+        <div className="flex justify-between mt-6">
+          <a className="uppercase border border-secondary-light bg-secondary-light bg-opacity-60 rounded-lg p-2 text-sm flex items-center gap-2 hover:opacity-60" href={liveURL} target="_blank">
+            Live Site 
+            <WebIcon />
           </a>
-          <br />
-          <a className="inline-block" href={codeURL} target="_blank">
-            <Button text="View Code" type="button" icon={<GitHubIcon />} />
+          <a className="uppercase border border-secondary-light bg-secondary-light bg-opacity-60 rounded-lg p-2 text-sm flex items-center gap-2 hover:opacity-60" href={codeURL} target="_blank">
+            Source Code 
+            <GitHubIcon />
           </a>
         </div>
       </div>
@@ -28,11 +29,12 @@ function ProjectCard({ project }) {
 
 ProjectCard.propTypes = {
   project: PropTypes.shape({
-    imgPath: PropTypes.string.isRequired,
+    backgroundClass: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     liveURL: PropTypes.string.isRequired,
-    codeURL: PropTypes.string.isRequired
+    codeURL: PropTypes.string.isRequired,
+    logoPath: PropTypes.string.isRequired,
   }).isRequired,
 }
 
--- src/components/sections/ProjectsSection.jsx
+++ src/components/sections/ProjectsSection.jsx
@@ -1,4 +1,4 @@
-import Project from "../projects/ProjectCard";
+import ProjectCard from "../projects/ProjectCard";
 import SectionContainer from "../shared/SectionContainer";
 
 function ProjectsSection() {
@@ -6,21 +6,24 @@ function ProjectsSection() {
     { 
       title: "Audiophile E-Commerce Website", 
       description: "An e-commerce website for purchasing high quality audio equipment.", 
-      imgPath: "/assets/projects/audiophile-ecommerce/audiophile-desktop.gif",
+      backgroundClass: "bg-audiophile-card bg-right",
+      logoPath: "/assets/projects/audiophile-ecommerce/audiophile-logo.svg",
       liveURL: "https://patrick-dolan.github.io/audiophile-ecommerce-website/",
       codeURL: "https://github.com/Patrick-Dolan/audiophile-ecommerce-website",
     },
     { 
       title: "Designo Agency Website", 
       description: "A design agency website showcasing a range of creative services.", 
-      imgPath: "/assets/projects/designo-company/designo-desktop.gif",
+      backgroundClass: "bg-designo-card bg-center",
+      logoPath: "/assets/projects/designo-company/designo-logo.png",
       liveURL: "https://patrick-dolan.github.io/designo-website/",
       codeURL: "https://github.com/Patrick-Dolan/designo-website",
     },
     { 
       title: "Space Tourism Website", 
       description: "A space tourism website where you can view destinations, the crew, and technology.", 
-      imgPath: "/assets/projects/space-tourism/space-tourism-desktop.gif",
+      backgroundClass: "bg-space-tourism-card bg-bottom",
+      logoPath: "/assets/projects/space-tourism/space-tourism-logo.svg",
       liveURL: "https://patrick-dolan.github.io/space-tourism-website/",
       codeURL: "https://github.com/Patrick-Dolan/space-tourism-website",
     },
@@ -31,9 +34,9 @@ function ProjectsSection() {
       <SectionContainer>
         <h2 className="text-center text-3xl mb-2 font-bold uppercase">Projects</h2>
         <p className="text-center text-sm mb-8">From Concept to Completion: My Work Portfolio</p>
-        <div className="space-y-10">
+        <div className="flex flex-col items-center gap-10">
           {projects.map((project, index) => (
-            <Project 
+            <ProjectCard 
               key={index} 
               project={project}
             />
