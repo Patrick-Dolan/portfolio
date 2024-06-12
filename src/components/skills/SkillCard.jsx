955e7e1e886a76683a8890666e31aa2c282c5592
Patrick Dolan <dolanp1992@gmail.com>
2024-06-12 09:09:56 -0700

    resize card and change title font weight

--- src/components/skills/SkillCard.jsx
+++ src/components/skills/SkillCard.jsx
@@ -2,9 +2,9 @@ import PropTypes from "prop-types";
 
 function SkillCard({ iconComponent, title }) {
   return (
-    <div className="space-y-2 w-24">
+    <div className="space-y-2 w-20">
       {iconComponent}
-      <p className="text-center font-bold">{title}</p>
+      <p className="text-center font-medium">{title}</p>
     </div>
   )
 }
