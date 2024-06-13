54dea4d8971d2f2b13784f8f9bfd2204e321dfee
Patrick Dolan <dolanp1992@gmail.com>
2024-06-13 13:24:52 -0700

    set large breakpoint max width

--- src/components/shared/SectionContainer.jsx
+++ src/components/shared/SectionContainer.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 function SectionContainer({ children }) {
   return (
-    <div className="px-6 md:px-0 md:max-w-172 md:mx-auto">
+    <div className="px-6 md:px-0 md:max-w-172 md:mx-auto lg:max-w-277">
       {children}
     </div>
   )
