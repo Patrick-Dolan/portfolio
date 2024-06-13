f3e1ccfea22408c827210321fca6cedc1191c7c4
Patrick Dolan <dolanp1992@gmail.com>
2024-06-13 10:53:33 -0700

    add tablet styles to footer

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -4,16 +4,18 @@ function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="py-10 text-center">
+    <footer className="py-10 text-center md:text-left">
       <SectionContainer>
-        <a href="#hero-section">
-          <p className="font-bold text-2xl">Patrick Dolan</p>
-        </a>
-        <ul className="uppercase text-sm my-6 flex justify-center gap-6">
-          <li><a href="#projects" className="tracking-widest hover:text-secondary">Projects</a></li>
-          <li><a href="#about" className="tracking-widest hover:text-secondary">About</a></li>
-          <li><a href="#contact" className="tracking-widest hover:text-secondary">Contact</a></li>
-        </ul>
+        <div className="md:flex md:justify-between md:items-center md:mb-4">
+          <a href="#hero-section">
+            <p className="font-bold text-2xl">Patrick Dolan</p>
+          </a>
+          <ul className="uppercase text-sm my-6 flex justify-center gap-6 md:my-0">
+            <li><a href="#projects" className="tracking-widest hover:text-secondary">Projects</a></li>
+            <li><a href="#about" className="tracking-widest hover:text-secondary">About</a></li>
+            <li><a href="#contact" className="tracking-widest hover:text-secondary">Contact</a></li>
+          </ul>
+        </div>
         <hr className="border-secondary-light opacity-50" />
         <p className="py-10">This site was built using React, TailwindCSS, and Firebase. If you would like to check out the code you can see it <a href="https://github.com/Patrick-Dolan/portfolio" target="_blank" className="text-secondary underline">here</a>.</p>
         <p>Copyright &copy; {currentYear} Patrick Dolan</p>
