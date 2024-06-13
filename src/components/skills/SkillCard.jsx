1515640691a3039097e2147fdb26f042b8c68cef
Patrick Dolan <dolanp1992@gmail.com>
2024-06-13 14:01:56 -0700

    change skill card width at large screens

--- src/components/skills/SkillCard.jsx
+++ src/components/skills/SkillCard.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 function SkillCard({ iconComponent, title }) {
   return (
-    <div className="space-y-2 w-20 md:w-28">
+    <div className="space-y-2 w-20 md:w-28 lg:w-24">
       {iconComponent}
       <p className="text-center font-medium md:text-xl">{title}</p>
     </div>
